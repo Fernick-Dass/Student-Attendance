f4b0ef2506e5b7f5b6ba1a42d212ff7c87a27666
Fernick <91066527+Fernick-Dass@users.noreply.github.com>
2024-01-16 14:01:15 +0530

    Add files via upload

---
+++ client/build/precache-manifest.f3bb287c458d996e494985daf63fa49a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67f9339de92e9d907bc7664d9162cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3067638d556f29364e27",
+    "url": "/static/css/main.120861a2.chunk.css"
+  },
+  {
+    "revision": "bb7110eb1975e335147c",
+    "url": "/static/js/2.314732bf.chunk.js"
+  },
+  {
+    "revision": "81e39deace866ac87fbcfb8577142737",
+    "url": "/static/js/2.314732bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3067638d556f29364e27",
+    "url": "/static/js/main.516330dd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.516330dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865d5e6dba3bde80deb1",
+    "url": "/static/js/runtime-main.e82bb23d.js"
+  },
+  {
+    "revision": "0e651ff6c9928dad413931ce4ed2057b",
+    "url": "/static/media/green-chameleon-s9CC2SKySJM-unsplash.0e651ff6.jpg"
+  }
+]);
